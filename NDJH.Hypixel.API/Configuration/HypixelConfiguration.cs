68200c8e8c782509ceb378349fbce1925d2829fd
Dante Hasty <dante9562@gmail.com>
2024-01-27 14:30:16 -0500

    hypixel config

---
+++ NDJH.Hypixel.API/Configuration/HypixelConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace NDJH.Hypixel.API.Configuration;
+
+public class HypixelConfiguration
+{
+    public string BaseUrl { get; set; } = "https://api.hypixel.net/v2";
+    public string ApiKey { get; set; } = string.Empty;
+}
