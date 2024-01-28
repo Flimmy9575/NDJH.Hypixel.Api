e8ac63dc598cee952f74608ff83d9883c294f4f9
Dante Hasty <dante9562@gmail.com>
2024-01-27 23:09:28 -0500

    Add guild achievements Model

---
+++ NDJH.Hypixel.API/Models/Resources/GuildAchievements.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NDJH.Hypixel.API.Models.Resources;
+
+public record GuildTier(
+    [property: JsonPropertyName("tier")] int TierValue,
+    [property: JsonPropertyName("amount")] int Amount);
+
+public record GuildTieredAchievement(
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("description")]
+    string Description,
+    [property: JsonPropertyName("tiers")] IReadOnlyList<GuildTier> Tiers);
+
+public record GuildAchievementResponse(
+    [property: JsonPropertyName("one_time")]
+    IReadOnlyDictionary<string, object> OneTime,
+    [property: JsonPropertyName("tiered")] IReadOnlyDictionary<string, GuildTieredAchievement> Tiered) : ResourcesBaseModel;
