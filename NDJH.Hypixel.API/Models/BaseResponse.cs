fb8d395baaa06465c848db0455c5f2eaf0618451
Dante Hasty <dante9562@gmail.com>
2024-01-27 19:08:03 -0500

    Add RecentGames Model Add BaseResponse Model

---
+++ NDJH.Hypixel.API/Models/BaseResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NDJH.Hypixel.API.Models;
+
+public record BaseResponse
+{
+    [JsonPropertyName("success")] public bool Success { get; set; }
+
+    [JsonPropertyName("uuid")] public Guid Guid { get; set; }
+};
---
+++ NDJH.Hypixel.API/Models/PlayerData/RecentGamesResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NDJH.Hypixel.API.Models.PlayerData;
+
+public record RecentGamesResponse(
+    [property: JsonPropertyName("games")] PlayedGames[] PlayedGames) : BaseResponse;
+
+public record PlayedGames(
+    [property: JsonPropertyName("date")] DateTime Date,
+    [property: JsonPropertyName("gameType")]
+    string GameType,
+    [property: JsonPropertyName("mode")] string? Mode,
+    [property: JsonPropertyName("map")] string? Map,
+    [property: JsonPropertyName("ended")] DateTime Ended);
