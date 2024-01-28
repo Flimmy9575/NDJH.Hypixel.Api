7ea377fc8d7fbb67b6555947424320c10842521f
Dante Hasty <dante9562@gmail.com>
2024-01-28 17:56:18 -0500

    Implemented methods from ISkyBlock interface into SkyBlock

--- NDJH.Hypixel.API/Services/SkyBlock.cs
+++ NDJH.Hypixel.API/Services/SkyBlock.cs
@@ -1,25 +1,85 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
+using NDJH.Hypixel.API.Models.SkyBlock;
 
 namespace NDJH.Hypixel.API.Services;
 
-public interface ISkyBlock
+public class SkyBlock : ISkyBlock
 {
-    
-}
-
-public class SkyBlock : ISkyBlock 
-{
-    private  readonly HttpClient __httpClient;
-    private readonly ILogger<SkyBlock> _logger; 
+    private readonly HttpClient __httpClient;
+    private readonly ILogger<SkyBlock> _logger;
 
     public SkyBlock(HttpClient httpClient, ILogger<SkyBlock> logger)
     {
         __httpClient = httpClient;
         _logger = logger;
     }
-    
+
     public static ISkyBlock Create(HttpClient httpClient, ILogger<SkyBlock>? logger = null) =>
         new SkyBlock(httpClient, logger ?? NullLogger<SkyBlock>.Instance);
-    
+
+    public Task<CollectionResponse> GetCollectionsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SkillsResponse> GetSkillsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ItemsResponse> GetItemsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ElectionsResponse> GetElectionsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BingoResponse> GetBingoAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<NewsResponse> GetNewsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AuctionResponse> GetAuctionAsync(string input, InputType inputType)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AuctionResponse> GetActiveAuctionsAsync(int page)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<AuctionsHistoryResponse> GetAuctionsHistoryAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BazaarResponse> GetBazaarAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProfileResponse> GetProfileAsync(string profileId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProfilesResponse> GetProfilesAsync(string uuid)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<BingoCompletionsResponse> GetBingoCompletionsAsync(string uuid)
+    {
+        throw new NotImplementedException();
+    }
 }
