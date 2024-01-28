b07b8c61b0074b9c22227a3708a377c329fd84ea
Dante Hasty <dante9562@gmail.com>
2024-01-27 23:01:19 -0500

    Add Vanity Pets and Companions Response

---
+++ NDJH.Hypixel.API/Models/Resources/VanityPets.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NDJH.Hypixel.API.Models.Resources;
+
+public record Type(
+    [property: JsonPropertyName("key")] string Key,
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("rarity")] string Rarity,
+    [property: JsonPropertyName("package")]
+    string Package);
+
+public record Rarity(
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("color")] string Color);
+
+public record VanityPetsAndCompanionsResponse(
+    [property: JsonPropertyName("types")] IReadOnlyList<Type> Types,
+    [property: JsonPropertyName("rarities")]
+    IReadOnlyList<Rarity> Rarities) : ResourcesBaseModel;
