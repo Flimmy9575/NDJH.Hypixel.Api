af366c5d9a446bb72c058d352a4823e65342d2a5
Dante Hasty <dante9562@gmail.com>
2024-01-27 23:12:28 -0500

    Add bingo and bingo completions Model

---
+++ NDJH.Hypixel.API/Models/SkyBlock/Bingo.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+using NDJH.Hypixel.API.Models.Resources;
+
+namespace NDJH.Hypixel.API.Models.SkyBlock;
+
+public record Goal(
+    [property: JsonPropertyName("id")] string Id,
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("lore")] string Lore,
+    [property: JsonPropertyName("tiers")] IReadOnlyList<int> Tiers,
+    [property: JsonPropertyName("progress")]
+    int Progress,
+    [property: JsonPropertyName("requiredAmount")]
+    int RequiredAmount);
+
+public record BingoResponse(
+    [property: JsonPropertyName("id")] int Id,
+    [property: JsonPropertyName("goals")] IReadOnlyList<Goal> Goals) : ResourcesBaseModel;
