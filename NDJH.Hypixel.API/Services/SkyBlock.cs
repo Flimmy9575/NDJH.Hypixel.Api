ad5d4a4804472a4fdc62e6f98fe5abe6f9aff847
Dante Hasty <dante9562@gmail.com>
2024-01-28 20:02:57 -0500

    Add method to request and serialize API response
    A new async method, RequestAndSerializeResponse, was added to SkyBlock.cs to handle HTTP GET requests. The function will throw an exception if the HTTP response is unsuccessful or if the returned data is null after deserialization.

--- NDJH.Hypixel.API/Services/SkyBlock.cs
+++ NDJH.Hypixel.API/Services/SkyBlock.cs
@@ -26,6 +26,42 @@ public class SkyBlock : ISkyBlock
     public Task<SkillsResponse> GetSkillsAsync()
     {
         throw new NotImplementedException();
+    private async Task<TResponse> RequestAndSerializeResponse<TResponse>(string url)
+    {
+        try
+        {
+            _logger.LogTrace("Requesting to {Path}.", url);
+
+            // Sending GET request to the collections endpoint
+            var response = await __httpClient.GetAsync(url);
+
+            // if the HTTP response status is an error status, this will throw an exception
+            response.EnsureSuccessStatusCode();
+
+            _logger.LogTrace("Received {Path} response. Deserializing.", url);
+
+            // Read the response content as a stream
+            var responseStream = await response.Content.ReadAsStreamAsync();
+
+            // Deserialize the response content stream into a CollectionResponse object
+            var data = await JsonSerializer.DeserializeAsync<TResponse>(responseStream);
+
+            if (data is null)
+            {
+                throw new ApiResponseNullException(
+                    $"Deserialization resulted in null. {typeof(TResponse)} object is not received as expected from the API.");
+            }
+
+            _logger.LogTrace("Deserialization complete.");
+
+            return data;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error retrieving information from Hypixel API({Path}).", url);
+            throw;
+        }
+    }
     }
 
     public Task<ItemsResponse> GetItemsAsync()
