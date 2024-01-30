eb9c4c2c940f22843bc6f03183c378248fc08a1d
Dante Hasty <dante9562@gmail.com>
2024-01-29 23:52:26 -0500

    Move RequestAndSerializeResponseAsync to it's own class and inject it into SkyBlock

--- NDJH.Hypixel.API/Services/SkyBlock.cs
+++ NDJH.Hypixel.API/Services/SkyBlock.cs
@@ -8,91 +8,49 @@ namespace NDJH.Hypixel.API.Services;
 
 public class SkyBlock : ISkyBlock
 {
-    private readonly HttpClient __httpClient;
-    private readonly ILogger<SkyBlock> _logger;
+    private readonly IHttpDeserializerService _httpRequestAndDeserializer; 
 
-    public SkyBlock(HttpClient httpClient, ILogger<SkyBlock> logger)
+    public SkyBlock(IHttpDeserializerService httpRequestAndDeserializer)
     {
-        __httpClient = httpClient;
-        _logger = logger;
+        _httpRequestAndDeserializer = httpRequestAndDeserializer;
     }
-
-    public static ISkyBlock Create(HttpClient httpClient, ILogger<SkyBlock>? logger = null) =>
-        new SkyBlock(httpClient, logger ?? NullLogger<SkyBlock>.Instance);
-
+    
     public async Task<CollectionResponse> GetCollectionsAsync() =>
-        await RequestAndSerializeResponseAsync<CollectionResponse>("/recources/skyblock/collections");
-
-    private async Task<TResponse> RequestAndSerializeResponseAsync<TResponse>(string url)
-    {
-        try
-        {
-            _logger.LogTrace("Requesting to {Path}.", url);
-
-            // Sending GET request to the collections endpoint
-            var response = await __httpClient.GetAsync(url);
-
-
-            // if the HTTP response status is an error status, this will throw an exception
-            response.EnsureSuccessStatusCode();
-
-            _logger.LogTrace("Received {Path} response. Deserializing.", url);
-
-            // Read the response content as a stream
-            var responseStream = await response.Content.ReadAsStreamAsync();
-
-            // Deserialize the response content stream into a CollectionResponse object
-            var data = await JsonSerializer.DeserializeAsync<TResponse>(responseStream);
-
-            if (data is null)
-            {
-                throw new ApiResponseNullException(
-                    $"Deserialization resulted in null. {typeof(TResponse)} object is not received as expected from the API.");
-            }
-
-            _logger.LogTrace("Deserialization complete.");
-
-            return data;
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error retrieving information from Hypixel API({Path}).", url);
-            throw;
-        }
-    }
+        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<CollectionResponse>("/recources/skyblock/collections");
+    
 
     public async Task<SkillsResponse> GetSkillsAsync() =>
-        await RequestAndSerializeResponseAsync<SkillsResponse>("/resources/skyblock/skills");
+        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<SkillsResponse>("/resources/skyblock/skills");
 
     public async Task<ItemsResponse> GetItemsAsync() =>
-        await RequestAndSerializeResponseAsync<ItemsResponse>("/resources/skyblock/items");
+        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ItemsResponse>("/resources/skyblock/items");
 
     public async Task<ElectionsResponse> GetElectionsAsync() =>
-        await RequestAndSerializeResponseAsync<ElectionsResponse>("/resources/skyblock/election");
+        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ElectionsResponse>("/resources/skyblock/election");
 
     public async Task<BingoResponse> GetBingoAsync() =>
-        await RequestAndSerializeResponseAsync<BingoResponse>("/resources/skyblock/bingo");
+        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<BingoResponse>("/resources/skyblock/bingo");
 
     public async Task<NewsResponse> GetNewsAsync() =>
-        await RequestAndSerializeResponseAsync<NewsResponse>("/resources/skyblock/news");
+        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<NewsResponse>("/resources/skyblock/news");
 
     public async Task<AuctionResponse> GetAuctionAsync(string input, InputType inputType) =>
-        await RequestAndSerializeResponseAsync<AuctionResponse>($"/skyblock/auction?{inputType}={input}");
+        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<AuctionResponse>($"/skyblock/auction?{inputType}={input}");
 
     public async Task<AuctionsResponse> GetActiveAuctionsAsync(int page) =>
-        await RequestAndSerializeResponseAsync<AuctionsResponse>($"/skyblock/auctions?page={page}");
+        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<AuctionsResponse>($"/skyblock/auctions?page={page}");
 
     public async Task<AuctionsHistoryResponse> GetAuctionsHistoryAsync() =>
-        await RequestAndSerializeResponseAsync<AuctionsHistoryResponse>("/skyblock/auctions_ended");
+        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<AuctionsHistoryResponse>("/skyblock/auctions_ended");
 
     public async Task<BazaarResponse> GetBazaarAsync() =>
-        await RequestAndSerializeResponseAsync<BazaarResponse>("skyblock/bazaar");
+        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<BazaarResponse>("skyblock/bazaar");
 
     public async Task<ProfileResponse> GetProfileAsync(string profileId) =>
-        await RequestAndSerializeResponseAsync<ProfileResponse>("/skyblock/profile");
+        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ProfileResponse>("/skyblock/profile");
 
-    public async Task<ProfilesResponse> GetProfilesAsync(string uuid) => await RequestAndSerializeResponseAsync<ProfilesResponse>("/skyblock/profiles");
+    public async Task<ProfilesResponse> GetProfilesAsync(string uuid) => await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ProfilesResponse>("/skyblock/profiles");
 
     public async Task<BingoCompletionsResponse> GetBingoCompletionsAsync(string uuid) =>
-        await RequestAndSerializeResponseAsync<BingoCompletionsResponse>($"/skyblock/bingo?uuid={uuid}");
+        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<BingoCompletionsResponse>($"/skyblock/bingo?uuid={uuid}");
 }
