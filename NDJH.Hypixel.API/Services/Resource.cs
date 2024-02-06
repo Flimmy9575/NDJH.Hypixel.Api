3fb2e6f86954a983d4321fa0f0b9b80c5fe0b7b1
Dante Hasty <dante9562@gmail.com>
2024-02-05 23:50:33 -0500

    Expand IResource interface and refactor Resource class
    The IResource interface was expanded with methods for retrieving achievements, challenges, quests, guild achievements, and vanity pets information. In order to implement this functionality, the Resource class was significantly refactored and each new method was implemented to make HTTP requests and deserialize the response.

--- NDJH.Hypixel.API/Services/Resource.cs
+++ NDJH.Hypixel.API/Services/Resource.cs
@@ -1,24 +1,69 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
+using NDJH.Hypixel.API.Models.Resources;
 
 namespace NDJH.Hypixel.API.Services;
 
 public interface IResource
 {
-    
+    /// <summary>
+    /// Retrieves the achievements data.
+    /// </summary>
+    /// <returns>An instance of AchievementResponse containing the achievements data.</returns>
+    public Task<AchievementResponse> GetAchievements();
+
+    /// <summary>
+    /// Retrieves the challenges data.
+    /// </summary>
+    /// <returns>An instance of ChallengesResponse containing the challenges data.</returns>
+    public Task<ChallengesResponse> GetChallenges();
+
+    /// <summary>
+    /// Retrieves the quests data.
+    /// </summary>
+    /// <returns>An instance of QuestsResponse containing the quests data.</returns>
+    public Task<QuestsResponse> GetQuests();
+
+    /// <summary>
+    /// Retrieves the guild achievements data.
+    /// </summary>
+    /// <returns>An instance of GuildAchievementResponse containing the guild achievements data.</returns>
+    public Task<GuildAchievementResponse> GetGuildAchievements();
+
+    /// <summary>
+    /// Retrieves the vanity pets and companions information.
+    /// </summary>
+    /// <returns>An instance of VanityPetsAndCompanionsResponse containing the vanity pets and companions data.</returns>
+    public Task<VanityPetsAndCompanionsResponse> GetVanityPetsInformation();
+
+    /// <summary>
+    /// Retrieves the information about vanity pets and companions.
+    /// </summary>
+    /// <returns>An instance of VanityPetsAndCompanionsResponse containing the vanity pets and companions data.</returns>
+    public Task<VanityPetsAndCompanionsResponse> GetCompanionInformation();
 }
 
-public class Resource : IResource
+public class Resource(IHttpDeserializerService httpRequestAndDeserializer) : IResource
 {
-    private  readonly HttpClient _httpClient;
-    private readonly ILogger<Resource> _logger; 
+    public async Task<AchievementResponse> GetAchievements() =>
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<AchievementResponse>(
+            "/resources/achievements");
 
-    public Resource(HttpClient httpClient, ILogger<Resource> logger)
-    {
-        _httpClient = httpClient;
-        _logger = logger;
-    }
-    
-    public static IResource Create(HttpClient httpClient, ILogger<Resource>? logger = null) =>
-        new Resource(httpClient, logger ?? NullLogger<Resource>.Instance);
+    public async Task<ChallengesResponse> GetChallenges() =>
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ChallengesResponse>("/resources/challenges");
+
+    public async Task<QuestsResponse> GetQuests() =>
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<QuestsResponse>("/resources/quests");
+
+    public async Task<GuildAchievementResponse> GetGuildAchievements() =>
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<GuildAchievementResponse>(
+            "/resources/guilds/achievements");
+
+    public async Task<VanityPetsAndCompanionsResponse> GetVanityPetsInformation() =>
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<VanityPetsAndCompanionsResponse>(
+            "/resources/vanity/pets");
+
+    public async Task<VanityPetsAndCompanionsResponse> GetCompanionInformation() =>
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<VanityPetsAndCompanionsResponse>(
+            "/resources/vanity/companions");
 }
