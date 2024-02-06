e5e0e07965c12b680e9c5ccab567aea443499b1b
Dante Hasty <dante9562@gmail.com>
2024-02-06 17:53:01 -0500

    Add base methods for the other endpoint()

--- NDJH.Hypixel.API/Services/Other.cs
+++ NDJH.Hypixel.API/Services/Other.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.Extensions.Logging.Abstractions;
-
-namespace NDJH.Hypixel.API.Services;
+﻿namespace NDJH.Hypixel.API.Services;
 
 public interface IOther
 {
-    
+    public Task GetBoosters();
+    public Task GetPlayerCounts();
+    public Task GetLeaderboards();
+    public Task GetPunishmentStats();
 }
 
 public class Other(IHttpDeserializerService httpRequestAndDeserializer) : IOther
 {
-    
+    public Task GetBoosters() => throw new NotSupportedException();
+
+    public Task GetPlayerCounts() => throw new NotSupportedException();
+    public Task GetLeaderboards() => throw new NotSupportedException();
+    public Task GetPunishmentStats() => throw new NotSupportedException();
 }
