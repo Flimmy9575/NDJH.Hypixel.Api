ab0402731f38f8d67b6e0ceec063191f57a0e60e
Dante Hasty <dante9562@gmail.com>
2024-02-05 23:49:53 -0500

    Refactor SkyBlock class to use primary constructor
    The SkyBlock class has been refactored to simplify its constructor by removing the private readonly field '_httpRequestAndDeserializer'. The IHttpDeserializerService parameter is now directly used within the class methods, eliminating redundancy and making the class more concise.

--- NDJH.Hypixel.API/Services/SkyBlock.cs
+++ NDJH.Hypixel.API/Services/SkyBlock.cs
@@ -6,51 +6,44 @@ using NDJH.Hypixel.API.Models.SkyBlock;
 
 namespace NDJH.Hypixel.API.Services;
 
-public class SkyBlock : ISkyBlock
+public class SkyBlock(IHttpDeserializerService httpRequestAndDeserializer) : ISkyBlock
 {
-    private readonly IHttpDeserializerService _httpRequestAndDeserializer; 
-
-    public SkyBlock(IHttpDeserializerService httpRequestAndDeserializer)
-    {
-        _httpRequestAndDeserializer = httpRequestAndDeserializer;
-    }
-    
     public async Task<CollectionResponse> GetCollectionsAsync() =>
-        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<CollectionResponse>("/recources/skyblock/collections");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<CollectionResponse>("/recources/skyblock/collections");
     
 
     public async Task<SkillsResponse> GetSkillsAsync() =>
-        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<SkillsResponse>("/resources/skyblock/skills");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<SkillsResponse>("/resources/skyblock/skills");
 
     public async Task<ItemsResponse> GetItemsAsync() =>
-        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ItemsResponse>("/resources/skyblock/items");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ItemsResponse>("/resources/skyblock/items");
 
     public async Task<ElectionsResponse> GetElectionsAsync() =>
-        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ElectionsResponse>("/resources/skyblock/election");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ElectionsResponse>("/resources/skyblock/election");
 
     public async Task<BingoResponse> GetBingoAsync() =>
-        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<BingoResponse>("/resources/skyblock/bingo");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<BingoResponse>("/resources/skyblock/bingo");
 
     public async Task<NewsResponse> GetNewsAsync() =>
-        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<NewsResponse>("/resources/skyblock/news");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<NewsResponse>("/resources/skyblock/news");
 
     public async Task<AuctionResponse> GetAuctionAsync(string input, InputType inputType) =>
-        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<AuctionResponse>($"/skyblock/auction?{inputType}={input}");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<AuctionResponse>($"/skyblock/auction?{inputType}={input}");
 
     public async Task<AuctionsResponse> GetActiveAuctionsAsync(int page) =>
-        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<AuctionsResponse>($"/skyblock/auctions?page={page}");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<AuctionsResponse>($"/skyblock/auctions?page={page}");
 
     public async Task<AuctionsHistoryResponse> GetAuctionsHistoryAsync() =>
-        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<AuctionsHistoryResponse>("/skyblock/auctions_ended");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<AuctionsHistoryResponse>("/skyblock/auctions_ended");
 
     public async Task<BazaarResponse> GetBazaarAsync() =>
-        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<BazaarResponse>("skyblock/bazaar");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<BazaarResponse>("skyblock/bazaar");
 
     public async Task<ProfileResponse> GetProfileAsync(string profileId) =>
-        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ProfileResponse>("/skyblock/profile");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ProfileResponse>("/skyblock/profile");
 
-    public async Task<ProfilesResponse> GetProfilesAsync(string uuid) => await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ProfilesResponse>("/skyblock/profiles");
+    public async Task<ProfilesResponse> GetProfilesAsync(string uuid) => await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<ProfilesResponse>("/skyblock/profiles");
 
     public async Task<BingoCompletionsResponse> GetBingoCompletionsAsync(string uuid) =>
-        await _httpRequestAndDeserializer.RequestAndSerializeResponseAsync<BingoCompletionsResponse>($"/skyblock/bingo?uuid={uuid}");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<BingoCompletionsResponse>($"/skyblock/bingo?uuid={uuid}");
 }
