391b75f4c63d27b32b0a3f23a652426f6d835910
Dante Hasty <dante9562@gmail.com>
2024-02-08 02:19:37 -0500

    Remove slashes

--- NDJH.Hypixel.API/Services/PlayerData.cs
+++ NDJH.Hypixel.API/Services/PlayerData.cs
@@ -5,14 +5,14 @@ namespace NDJH.Hypixel.API.Services;
 public class PlayerData(IHttpDeserializerService httpRequestAndDeserializer) : IPlayerData
 {
     public async Task<Player> GetPlayerAsync(string uuid) =>
-        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<Player>($"/player?uuid={uuid}");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<Player>($"player?uuid={uuid}");
 
     public async Task<RecentGamesResponse> GetRecentGamesAsync(string uuid) =>
         await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<RecentGamesResponse>(
-            $"/recentgames?uuid={uuid}");
+            $"recentgames?uuid={uuid}");
 
     public async Task<StatusResponse> GetStatusAsync(string uuid) =>
-        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<StatusResponse>($"/status?uuid={uuid}");
+        await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<StatusResponse>($"status?uuid={uuid}");
 
     public async Task<GuildResponse> GetGuildAsync(string input, InputType inputType) =>
         await httpRequestAndDeserializer.RequestAndSerializeResponseAsync<GuildResponse>($"guild?{inputType}={input}");
