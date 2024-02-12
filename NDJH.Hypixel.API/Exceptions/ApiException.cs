cc478617b059c3bd59fef3655620f160d3fdc051
Dante Hasty <dante9562@gmail.com>
2024-02-11 21:06:19 -0500

    Add new ApiException class

---
+++ NDJH.Hypixel.API/Exceptions/ApiException.cs
@@ -0,0 +1,15 @@
+﻿namespace NDJH.Hypixel.API.Exceptions;
+
+public class ApiException : Exception
+{
+    public ApiException(int statusCode, string content)
+        : base($"Server returned status code: {statusCode}. Response content: {content}")
+    {
+        StatusCode = statusCode;
+        Content = content;
+    }
+
+    public int StatusCode { get; private set; }
+
+    public string Content { get; private set; }
+}
