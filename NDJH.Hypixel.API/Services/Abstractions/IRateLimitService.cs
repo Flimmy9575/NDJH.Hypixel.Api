fbae17fcbca9052e79721b91a100877638e1adbf
Dante Hasty <dante9562@gmail.com>
2024-02-11 22:51:48 -0500

    Add IRateLimitService interface
    The new interface, IRateLimitService, has been added to manage the rate limit of the Hypixel API calls.

---
+++ NDJH.Hypixel.API/Services/Abstractions/IRateLimitService.cs
@@ -0,0 +1,7 @@
+﻿namespace NDJH.Hypixel.API.Services.Abstractions;
+
+public interface IRateLimitService
+{
+    Task WaitForRateLimitAsync(CancellationToken cancellationToken);
+    void SetRateLimit(int remaining, int resetsInSeconds);
+}
