d62e6a323ad708b4ca2fe87618ad530c5502d8db
Dante Hasty <dante9562@gmail.com>
2024-02-15 23:27:15 -0500

    Add NDJH.Hypixel.API.Tests.Shared project and adjust references
    This update introduces a new project, NDJH.Hypixel.API.Tests.Shared, to the solution. The `TestConstants.cs` file has been moved from `NDJH.Hypixel.API.Tests` to `NDJH.Hypixel.API.Tests.Shared`. Furthermore, added NSubstitute to `NDJH.Hypixel.API.IntegrationTests`.

---
+++ NDJH.Hypixel.API.IntegrationTests/TestsUtility.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using NDJH.Hypixel.API.Services.Abstractions;
+using NSubstitute;
+
+namespace NDJH.Hypixel.API.IntegrationTests;
+
+public static class TestsUtility
+{
+    public static async Task<(IHttpDeserializerService, T)> SetupMockServiceAsync<T>(string jsonFilePath)
+        where T : class
+    {
+        var mockService = Substitute.For<IHttpDeserializerService>();
+
+        var json = await File.ReadAllTextAsync(jsonFilePath);
+        var expectedObject = JsonSerializer.Deserialize<T>(json);
+
+        mockService.RequestAndSerializeResponseAsync<T>(Arg.Any<string>(), Arg.Any<CancellationToken>())!
+            .Returns(expectedObject);
+
+        return (mockService, expectedObject);
+    }
+}
